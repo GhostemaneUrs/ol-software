ef67dfc0699a5c43d2b479c3a2a7c02a95310de2
GhostemaneUrs <JaramilloG1999@outlook.com>
2023-09-08 09:39:33 -0500

    feat: create root reducer

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,8 +1,16 @@
 import auth from './slices/auth'
+import user from './slices/user'
+import project from './slices/project'
+import settings from './slices/settings'
+import dashboard from './slices/dashboard'
 import { combineReducers } from '@reduxjs/toolkit'
 
 const rootReducer = combineReducers({
   auth,
+  user,
+  settings,
+  project,
+  dashboard,
 })
 
 export default rootReducer
